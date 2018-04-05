6c81a2f68c02d73652812ef28d4555ea808fa649
shellslau <michelle@tower3.com>
2018-04-05 13:39:28 -0700

    added moveUp and moveDown functions to the queue - untested

--- queue.js
+++ queue.js
@@ -1,11 +1,14 @@
 //working on the queue, I am trying to just write one that stores
 //a number(the song id) and that can have pieces of the queue move
 
+//I tried to add a function to move a specific song id up the queue
+//and one to move the id down the queue but I had trouble keeping track
+//of what was moving where so I am not sure if it worked properly
+
 //id = song id
 //prev = song before it in the queue
 //next = song after it in the queue
-//spot = spot in queue (i'm not sure if this is necessary yet)
-function Node(id, prev, next, spot){
+function Node(id, prev, next){
     this.id = id;
     this.prev = prev;
     this.next = next;
@@ -19,7 +22,7 @@ function Queue(){
     this.size = 0;
 
     this.push = function(id) {
-        let node = new Node(id, this.bottom, null, 0);
+        let node = new Node(id, this.bottom, null);
         if (this.bottom){
             this.bottom.next = node;
         }
@@ -37,6 +40,37 @@ function Queue(){
         this.top.prev = null;
         this.size--;
         return node;
+    }
+
+    //moves a song up the queue by 1
+    //I think the swapping is correct but I haven't tested it
+    this.moveUp = function(id){
+        let node = this.top;
+        while(node.id != id){
+            node = node.next;
+        }
+
+        node.prev.next = node.next;
+        node.prev = node.prev.prev;
+        node.next = node.prev;
+        node.next.prev = node;
+        node.next.next.prev = node.next;
+        node.prev.next = node;
+    }
+
+    //moves a song down the queue by 1
+    this.moveDown = function(id){
+        let node = this.top;
+        while(node.id != id){
+            node = node.next;
+        }
+
+        node.next.prev = node.prev;
+        node.prev = node.next;
+        node.next = node.prev.next;
+        node.prev.next = node;
+        node.next.prev = node;
+        node.prev.prev.next = node.prev;
     }
 }
 
