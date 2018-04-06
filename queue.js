351aecab66116bc16807144a76285a17a21a44aa
Kai Schniedergers <kschnied@ucsc.edu>
2018-04-05 21:23:32 -0700

    put all the code together!!!

--- queue.js
+++ queue.js
@@ -34,8 +34,48 @@ function Queue(){
 
     };
 
+    this.ammendInsert = function(insertNode, prevNode, nextNode) {
+
+        // nothing in queue, just add it to queue
+        if(prevNode == null && nextNode == null) {
+            this.bottom = insertNode;
+            this.top = insertNode;
+            this.size = 1;
+        }
+        // previous node is null, meaning it is being added to top of queue
+        else if(prevNode == null) {
+            this.top = insertNode;
+            insertNode.next = nextNode;
+            insertNode.prev = null;
+            nextNode.prev = insertNode;
+            this.size++;
+        }
+        // next node is null, meaining it is being added to bottom of queue
+        else if(nextNode == null) {
+            this.bottom = insertNode;
+            insertNode.next = null
+            insertNode.prev = prevNode;
+            prevNode.next = insertNode;
+            this.size++;
+        }
+        // adding to middle of queue
+        else {
+            insertNode.prev = prevNode;
+            insertNode.next = nextNode;
+            prevNode.next = insertNode;
+            nextNode.prev = insertNode;
+            this.size++;
+        }
+    };
+
     this.pop = function(){
         let node = this.top;
+        if (this.size === 0){
+            this.top = null;
+            this.bottom = null;
+            this.size--;
+            return node;
+        }
         this.top = node.next;
         this.top.prev = null;
         this.size--;
@@ -57,6 +97,29 @@ function Queue(){
         node.next.next.prev = node.next;
         node.prev.next = node;
     };
+
+    //fixes nodes around node to remove or move
+    this.ammendRemoval = function(nodeToRemove) {
+        //If the node is by itself
+        if (nodeToRemove.prev == null && nodeToRemove.next == null){
+            this.pop();
+        }
+        //If the node is the head
+        if (nodeToRemove.prev == null) {
+            this.pop();
+        }
+        //If the node is the tail
+        if (nodeToRemove.next == null) {
+            nodeToRemove.prev.next = null;
+        }
+        //If both sides are not null
+        else {
+            nodeToRemove.next.prev = nodeToRemove.prev;
+            nodeToRemove.prev.next = nodeToRemove.next;
+            this.size--;
+        }
+
+    }
 
     //moves a song down the queue by 1
     this.moveDown = function(id){
