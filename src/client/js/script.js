036ee8b81c03a661679b3fec2e853d07e1db088f
GEM YABUT <gemyabut@eduroam-169-233-254-190.ucsc.edu>
2018-05-17 00:26:51 -0700

    Fixed redirection from signup to home

--- src/client/js/script.js
+++ src/client/js/script.js
@@ -88,10 +88,12 @@ function signUpPage() {
                 $("#password-confirm-section").val('');
             }
             else {
+                var url = "/home.html";
                 console.log("SUCCESS");
                 console.log(data);
                 sessionStorage.setItem("username", data.username);
                 sessionStorage.setItem("loginCode", data.loginCode);
+                $(location).attr('href',url);
             }
         });
   });
