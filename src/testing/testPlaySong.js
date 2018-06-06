0c7473f2406deba42f1283146eadf517bd8d8058
Kristopher Rollert <krollert@ucsc.edu>
2018-06-05 23:55:25 -0700

    checking updates

--- src/testing/testPlaySong.js
+++ src/testing/testPlaySong.js
@@ -7,21 +7,21 @@ describe('Server', function() {
 
             request.get('http://localhost:3000/party/not-a-party/play', function(error, response, body) {
                 assert.equal(JSON.parse(body).error, "Party not found!");
-            })
+            });
         });
 
         it('returns message when queue is empty', function () {
 
             request.get('http://localhost:3000/party/SKczvnOb/play', function(error, response, body) {
                 assert.equal(JSON.parse(body).error, "Queue is empty!");
-            })
+            });
         });
 
         it('returns message when next song should be playing', function() {
 
             request.get('http://localhost:3000/party/VgyRRrtc/play', function(error, response, body) {
                 assert.equal(JSON.parse(body).message, "Playing Song...");
-            })
+            });
         });
     });
 });
